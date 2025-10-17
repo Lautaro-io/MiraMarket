e3433e72135a93d9054337afe311b017af719e38
Lautaro-io <ildarrazlautaro@gmail.com>
2025-10-17 17:28:36 -0300

    Prepare the project for develop

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,16 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+    id("com.google.gms.google-services")
+    id("com.google.firebase.crashlytics")
+    id("com.google.dagger.hilt.android")
+    alias(libs.plugins.ksp)
+
+
 }
 
 android {
@@ -33,9 +42,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
     }
+
     buildFeatures {
         compose = true
     }
@@ -57,6 +69,31 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //FIREBASE
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashlytics.ndk)
+    implementation(libs.google.firebase.analytics)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.auth)
+
+    //HILT
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.androidx.hilt.compiler)
+    //KAMEL & COIL
+    implementation(libs.kamel.image)
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network.okhttp)
+    //WORKER
+    implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.hilt.work)
+
+    implementation(libs.androidx.core.splashscreen)
+    implementation(libs.play.services.auth)
+
+
+
+
+
 }
---
+++ app/src/main/java/com/chelo/miramarket/domain/model/TimeRange.kt
@@ -0,0 +1,8 @@
+package com.chelo.miramarket.domain.model
+
+import java.time.LocalTime
+
+data class TimeRange(
+    val start: LocalTime,
+    val end: LocalTime
+)

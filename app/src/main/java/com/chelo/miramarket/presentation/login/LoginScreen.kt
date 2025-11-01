e6e866af54e1340af6cfdfe406397a72740828fb
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2025-11-01 19:39:42 -0300

    Se creo la LoginScreen

---
+++ app/src/main/java/com/chelo/miramarket/presentation/login/LoginScreen.kt
@@ -0,0 +1,28 @@
+package com.chelo.miramarket.presentation.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+
+@Composable
+fun LoginScreen(navigate: () -> Unit) {
+    LaunchedEffect(Unit)
+    {
+        delay(5000L)
+        navigate()
+    }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {navigate()}) {Text("Ingresar con Google") }
+    }
+}

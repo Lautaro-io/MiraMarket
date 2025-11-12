67f3e49e64fe0588f72fac7a40a275581bfa8c5d
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2025-11-12 18:59:22 -0300

    Se agregaron las dependencias de OsmAnd y MapLibre

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.play.services.location)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -101,7 +102,15 @@ dependencies {
     implementation(libs.androidx.credentials.play.services.auth)
     implementation(libs.googleid)
 
+    implementation(libs.osmdroid.android)
+    implementation(libs.play.services.location)
+    implementation(libs.maps.compose.widgets)
+    implementation(libs.maps.compose)
+    implementation(libs.maps.compose.utils)
+    implementation(libs.maps.compose.widgets)
 
+    implementation(libs.android.sdk)
+    implementation(libs.android.plugin.annotation.v9)
 
 
 

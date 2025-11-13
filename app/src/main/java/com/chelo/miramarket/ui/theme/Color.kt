0acda022ce6edfa36b4a1b974cb77ed84002a999
Lautaro Ildarraz <112969613+Lautaro-io@users.noreply.github.com>
2025-11-13 16:27:34 -0300

    Agregue unos colores

--- app/src/main/java/com/chelo/miramarket/ui/theme/Color.kt
+++ app/src/main/java/com/chelo/miramarket/ui/theme/Color.kt
@@ -11,3 +11,12 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
 val Background = Color(0xFFf5F7F7)
+
+val BackgroundBlack = Color(0xFF000000)
+
+val SplashBackground = Color(0xFF0F111F)
+
+val OpenStore = Color(0xFF49904B)
+val OpenSoonStore = Color(0xFFAC755A)
+
+val FieldBg = Color(0xFFC9C9C9)
